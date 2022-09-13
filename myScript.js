51f1abd68da9a90933b22dfeb6d14880496d4457
Luz42 <a.evangelista93@gmail.com>
2022-09-13 14:05:07 +0200

    importato il markup di HTML & CSS
    scritto pseudocodice in JS
    
    START

--- myScript.js
+++ myScript.js
@@ -1,1 +1,29 @@
 console.log('JS-OK');
+
+/*
+dato un array di immagini
+
+assegno v-show alle immagini
+
+assegno con un valore booleano (v-show) la classe attiva ad una immagine presente nell'arrray andandola a puntare [0]
+
+let counter = 0
+@click button(){
+    if(counter < array.lenght){
+
+        counter++
+
+
+    }else{
+        counter = 0
+    }
+
+    indexActive = array[counter]
+
+} 
+
+
+quando clicco su un bottone questa classe attiva cambierà quella corrente e quella successiva o precedente
+
+il bottone avrà un contatore che andrà ad aumentare il valore fino al <array.lenght
+*/
